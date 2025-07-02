669b1b361774b51243c6e6acfd00213367fd72e0
ikotome <kotomizu.0728@gmail.com>
2025-07-02 17:27:07 +0900

    Chartkick か Chart.jsが悪さしてるに決まってる

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # HTMLページ表示
-  get '/access_points', to: 'access_points#index'
+  get "/access_points", to: "access_points#index"
   # get '/client_logs', to: 'client_logs#index'
-  get '/dashboard', to: 'dashboard#index'
+  get "/dashboard", to: "dashboard#index"
 
   # MerakiのAP一覧をCSVに保存する用
-  get '/access_points/sync', to: 'access_points#sync'
+  get "/access_points/sync", to: "access_points#sync"
   # クライアントログをCSVに保存する用
-  get '/client_logs/sync', to: 'client_logs#sync'
-
+  get "/client_logs/sync", to: "client_logs#sync"
 end
